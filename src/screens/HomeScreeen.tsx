357b35d2a1b90719a845cc5f0bea29ca3314278a
Vladimir Saldivar <vladimir.saldivar@morsco.com>
2023-01-22 00:11:58 -0600

    modify: navigator and home
    modifico el navigator y agrego la imagen de pokemon

--- src/screens/HomeScreeen.tsx
+++ src/screens/HomeScreeen.tsx
@@ -1,10 +1,29 @@
-import React from 'react'
-import { Text, View } from 'react-native'
+import React from 'react';
+import {Image, Text, View} from 'react-native';
+import {useSafeAreaInsets} from 'react-native-safe-area-context';
+import {styles} from '../theme/appTheme';
 
 export const HomeScreen = () => {
+  //destructurar el top del safe area
+  const {top} = useSafeAreaInsets();
+
   return (
     <View>
-        <Text>HomeScreen</Text>
+      <Image
+        source={require('../assets/pokebola.png')}
+        style={styles.pokebolaBG}
+      />
+
+      <Text
+        style={{
+          //destructuro las propiedades
+          //primero las desestructuraciones y luego las modificaciones
+          ...styles.globalMargin,
+          ...styles.gnralTittle,
+          top: top + 20,
+        }}>
+        Pokedex
+      </Text>
     </View>
-  )
-}
+  );
+};
