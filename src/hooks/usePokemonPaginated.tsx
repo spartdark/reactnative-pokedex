570dcd87a2ec50eddf80b8269d44445dda136a8a
Vladimir Saldivar <vladimir.saldivar@morsco.com>
2023-01-22 22:59:46 -0600

    infinite scroll, fade in image

--- src/hooks/usePokemonPaginated.tsx
+++ src/hooks/usePokemonPaginated.tsx
@@ -1,20 +1,53 @@
-import {useEffect, useRef} from 'react';
+import {useEffect, useRef, useState} from 'react';
 import {pokemonAPI} from '../api/pokemonApi';
+import {
+  PokemonResponse,
+  SimplePokemon,
+  Result,
+} from '../interfaces/pokemonInterfaces';
 
 export const usePokemonPaginated = () => {
-  //const baseURL = 'https://pokeapi.co/api/v2/pokemon?offset=20&limit=40';
+  const [isLoading, setIsLoading] = useState(true);
 
+  const [simplePokemonList, setSimplePokemonList] = useState<SimplePokemon[]>(
+    [],
+  );
   const nextPageURL = useRef(
     'https://pokeapi.co/api/v2/pokemon?offset=20&limit=40',
   );
 
   //const nextPageURL = ''
   const loadPokemons = async () => {
-    const resp = await pokemonAPI.get(nextPageURL.current);
+    setIsLoading(true);
+    const resp = await pokemonAPI.get<PokemonResponse>(nextPageURL.current);
     console.log(resp.data);
+    //uso el useRef para actualizar la siguiente url a consumir
+    nextPageURL.current = resp.data.next;
+    //obtengo los resultados de la peticion http
+    mapPokemonList(resp.data.results);
+  };
+
+  const mapPokemonList = (pokemonList: Result[]) => {
+    //pokemonList.forEach(myPokemon => console.log(myPokemon));
+    const newPokemonList: SimplePokemon[] = pokemonList.map(({name, url}) => {
+      const urlParts = url.split('/');
+      const pokemonId = urlParts[urlParts.length - 2];
+      const pokemonPicture = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemonId}.png`;
+
+      return {pokemonId, pokemonPicture, name};
+    });
+    //acumulo los pokemones anteriores mas los nuevos
+    setSimplePokemonList([...simplePokemonList, ...newPokemonList]);
+    setIsLoading(false);
   };
 
   useEffect(() => {
     loadPokemons();
   }, []);
+
+  return {
+    simplePokemonList,
+    isLoading,
+    loadPokemons,
+  };
 };
--- src/screens/HomeScreeen.tsx
+++ src/screens/HomeScreeen.tsx
@@ -1,13 +1,18 @@
 import React from 'react';
-import {Image, Text, View} from 'react-native';
+import {ActivityIndicator, Image, Text, View} from 'react-native';
 import {useSafeAreaInsets} from 'react-native-safe-area-context';
 import {usePokemonPaginated} from '../hooks/usePokemonPaginated';
 import {styles} from '../theme/appTheme';
+import {SimplePokemon} from '../interfaces/pokemonInterfaces';
+import {FlatList} from 'react-native-gesture-handler';
+import {pokemonAPI} from '../api/pokemonApi';
+import {FadeInImage} from '../components/FadeInImage';
 
 export const HomeScreen = () => {
   //destructurar el top del safe area
   const {top} = useSafeAreaInsets();
-  usePokemonPaginated();
+  const {simplePokemonList, loadPokemons} = usePokemonPaginated();
+  console.log(`HomeScreen() => ${simplePokemonList}`);
 
   return (
     <View>
@@ -15,8 +20,28 @@ export const HomeScreen = () => {
         source={require('../assets/pokebola.png')}
         style={styles.pokebolaBG}
       />
-
-      <Text
+      <FlatList
+        data={simplePokemonList}
+        keyExtractor={pokemonAPI => pokemonAPI.pokemonId}
+        showsVerticalScrollIndicator={false}
+        //renderItem={({item}) => <Text>{item.name}</Text>}
+        renderItem={({item}) => (
+          <FadeInImage
+            uri={item.pokemonPicture}
+            //source={{uri: item.pokemonPicture}}
+            style={{
+              width: 100,
+              height: 100,
+            }}></FadeInImage>
+        )}
+        //infinite Scrool
+        onEndReached={loadPokemons}
+        onEndReachedThreshold={0.4}
+        ListFooterComponent={
+          <ActivityIndicator style={{height: 100}} size={20} color="grey" />
+        }
+      />
+      {/* <Text
         style={{
           //destructuro las propiedades
           //primero las desestructuraciones y luego las modificaciones
@@ -25,7 +50,7 @@ export const HomeScreen = () => {
           top: top + 20,
         }}>
         Pokedex
-      </Text>
+      </Text> */}
     </View>
   );
 };
